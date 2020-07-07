81fe9dcfd09efd8c14462a342c425279d6fd2fda
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2020-07-07 18:05:57 +0300

    Get version from git tags, remove file version

--- setup.py
+++ setup.py
@@ -1,21 +1,42 @@
+import subprocess
+
 from setuptools import setup
 
-with open('README.md') as fh:
-    long_description = fh.read()
 
-with open('version') as fh:
-    version = fh.read().splitlines()[0]
+def get_version() -> str:
+    def version_from_file():
+        with open('version') as fd:
+            return fd.read().splitlines()[0]
+
+    def version_from_git():
+        cmd = ['git', 'describe', '--abbrev=0', '--tags']
+        try:
+            return subprocess.check_output(cmd).decode().splitlines()[0]
+        except Exception as e:
+            print('ERROR! Execute {}: {}'.format(cmd, e))
+            return None
+    version = version_from_git()
+    if not version:
+        version = version_from_file()
+        print('WARNING! Get version from a file: {}'.format(version))
+    return version
+
+
+def get_long_description():
+    with open('README.md') as fh:
+        return fh.read()
+
 
 setup(
     name='rhvoice-wrapper',
-    version=version,
+    version=get_version(),
     packages=['rhvoice_wrapper'],
     url='https://github.com/Aculeasis/rhvoice-proxy',
     license='GPLv3+',
     author='Aculeasis',
     author_email='amilpalimov2@ya.ru',
     description='High-level interface for RHVoice library',
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type='text/markdown',
     python_requires='>=3.4',
     extras_require={
